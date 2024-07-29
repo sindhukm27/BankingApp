18c2e9db8fe29eb2f24143777db412ba1e701cb8
sindhukm27 <sindhu.kodoor@gmail.com>
2024-07-29 11:50:58 +1000

    Add files via upload
    first commit

---
+++ bankingApp/src/test/java/com/spring/bankingApp/BankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.bankingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
